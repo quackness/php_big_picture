825a747f77fc0f435c9cfde0d55630721fc8cc9f
Karolina Redden <dubajkaro@gmail.com>
2023-02-14 21:08:17 -0400

    add first php file

---
+++ HelloWorld.php
@@ -0,0 +1,15 @@
+<?php
+$message = "Hello World hi ";
+$title = "HTML Sample";
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Fundamentals - <?=$title?></title>
+    </head>
+    <body>
+        <h1><?php echo $message ?></h1>	
+    </body>
+</html>
+
---
+++ testing.php
@@ -0,0 +1,12 @@
+<?php
+
+$a="5";
+$b=4;
+
+echo ($a + $b).PHP_EOL;
+
+$a="Hello ";
+$b="World";
+
+echo $a, $b .PHP_EOL;
+
